34ddc210b45dc5e6256c0ca0105f741234fd0040
Michael Shuff <mjshuff23@gmail.com>
2023-09-05 11:21:35 -0400

    on styles

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,21 @@
+import Head from 'next/head';
 import Link from 'next/link';
+import Script from 'next/script';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+        {/* Do Do */}
+      </Head>
+      {/* next/script should not be wrapped in next/head */}
+      <Script
+        src='https://connect.facebook.net/en_US/sdk.js'
+        strategy='lazyOnload'
+        onLoad={() => console.log(`script loaded correctly, window.FB has been populated`)}
+      />
       <h1>First Post!</h1>
       <h2>
         {/* The Link component enables client-side navigation between two pages in the same Next.js app.
@@ -15,6 +28,6 @@ export default function FirstPost() {
         <Link href='/'>Back to Home</Link>
         {/* If you’ve used <a href="…"> instead of <Link href="…"> and did this, the background color will be cleared on link clicks because the browser does a full refresh. */}
       </h2>
-    </>
+    </Layout>
   );
 }
